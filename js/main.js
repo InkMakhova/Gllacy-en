238ae05df3e1b6c440bce0cc3fd21bd35d213ae3
Inna <i.m.makhova@gmail.com>
2021-12-01 17:03:06 +0200

    Project initialization (english version)

---
+++ js/main.js
@@ -0,0 +1,76 @@
+const body = document.querySelector('body');
+
+const BackgroundColors = {
+  GREEN: '#849D8F',
+  BLUE: '#8996A6',
+  GREY: '#9D8B84',
+}
+
+//слайдер
+let slides = body
+  .querySelectorAll('.slide-item');
+
+let controls = body
+  .querySelector('.slider-controls')
+  .querySelectorAll('input');
+
+const setBackgroundColor = (index) => {
+  switch (index) {
+    case 0:
+      return BackgroundColors.GREEN;
+    case 1:
+      return BackgroundColors.BLUE;
+    case 2:
+      return BackgroundColors.GREY;
+    default:
+      BackgroundColors.GREEN;
+  }
+}
+
+controls.forEach((control, index) => {
+  control.addEventListener('change', () => {
+    controls.forEach(control => control.checked = false);
+    slides.forEach(slide => slide.classList.remove('show'));
+    control.checked = true;
+    slides[index].classList.add('show');
+    body.style.backgroundColor = setBackgroundColor(index);
+  });
+});
+
+//модальное окно
+const modal = body.querySelector('.modal-feedback');
+
+const showModalButton = body.querySelector('.button-feedback');
+const closeModalButton = modal.querySelector('.modal-close');
+
+const clickOpenHandler = (evt) => {
+  evt.preventDefault();
+  modal.classList.add('modal-show');
+  modal.removeEventListener('click', clickOpenHandler);
+  showModalButton.classList.add('disabled');
+
+  closeModalButton.addEventListener('click', clickCloseHandler);
+
+  window.addEventListener('keydown', escDownHandler);
+}
+
+const clickCloseHandler = (evt) => {
+  evt.preventDefault();
+  modal.classList.remove('modal-show');
+  closeModalButton.removeEventListener('click', clickCloseHandler);
+  showModalButton.classList.remove('disabled');
+}
+
+const escDownHandler = (evt) => {
+  if (evt.key === 'Esc' || evt.key === 'Escape') {
+    modal.classList.remove('modal-show');
+    showModalButton.classList.remove('disabled');
+
+    closeModalButton.removeEventListener('click', clickCloseHandler);
+    window.addEventListener('keydown', escDownHandler);
+  }
+}
+
+showModalButton.addEventListener('click', clickOpenHandler);
+
+
